eff2705de56d623122f06bb700c8f21b2c9b4d15
prassurohith2212 <129072511+prassurohith2212@users.noreply.github.com>
2023-06-26 14:04:51 +0800

    ec2.tf

--- My Terraform Modules/modules/ec2/ec2.tf
+++ My Terraform Modules/modules/ec2/ec2.tf
@@ -1,1 +1,23 @@
+# Create a EC2 Instance (Ubuntu 20)
+resource "aws_instance" "node" {
+  instance_type          = "t2.micro" # free instance
+  ami                    = "ami-0d527b8c289b4af7f"
+  key_name               = aws_key_pair.key_pair.id
+  vpc_security_group_ids = [var.public_sg]
+  subnet_id              = var.public_subnet
 
+  tags = {
+    Name = "TF Generated EC2"
+  }
+
+  user_data = file("${path.root}/ec2/userdata.tpl")
+
+  root_block_device {
+    volume_size = 10
+  }
+}
+
+# Create and assosiate an Elastic IP
+resource "aws_eip" "eip" {
+  instance = aws_instance.node.id
+}
