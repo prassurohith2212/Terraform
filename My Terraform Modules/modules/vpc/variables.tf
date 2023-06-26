e0e7fbac4ae9fb87e01c5a01eee43da91d72c697
prassurohith2212 <129072511+prassurohith2212@users.noreply.github.com>
2023-06-26 14:28:19 +0800

    Update variables.tf

--- My Terraform Modules/modules/vpc/variables.tf
+++ My Terraform Modules/modules/vpc/variables.tf
@@ -1,1 +1,35 @@
+# Variables for Creating VPC:
+
+# 1. Variable for default region
+
+variable "aws_region" {
+  description = "Region in which AWS Resources to be created"
+  type = string
+  default = "ap-southeast-1"  
+}
+
+# 2. Availability Zones for Creating VPC# VPC Availability Zones
+variable "vpc_availability_zones" {
+  description = "VPC Availability Zones"
+  type = list(string)
+  default = ["ap-southeast-1a", "ap-southeast-1b"]
+}
+
+# 3.  VPC Name:
+
+variable "vpc_name" {
+  description = "VPC Name"
+  type = string 
+  default = "myvpc"
+}
+
+# 4.VPC CIDR Block
+variable "vpc_cidr_block" {
+  description = "VPC CIDR Block"
+  type = string 
+  default = "10.0.0.0/16"
+}
+
+
+
 
